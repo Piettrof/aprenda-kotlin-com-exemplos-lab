a404672637d9bc4bc1ed203a72c6c10726c60032
Piettrof <117880320+Piettrof@users.noreply.github.com>
2023-10-10 09:29:25 -0300

    conteudo dos curso

--- desafio.kt
+++ desafio.kt
@@ -4,7 +4,7 @@ enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 120)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
@@ -21,9 +21,14 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 fun main() {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+//Alunos
     val usuario1 = Usuario("Kotin Brian")
     val usuario2 = Usuario("DiJavan")
     val usuario3 = Usuario("Gal  Godot")
 
 
+// Materias 
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 60)
+    val conteudo2 = ConteudoEducacional("Banco de Dados", 60)
+    val conteudo3 = ConteudoEducacional("Desenvolvimento Web", 60)
 }
