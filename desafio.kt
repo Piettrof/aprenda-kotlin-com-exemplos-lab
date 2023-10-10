4737c8c74d4a67f17654bd4a013b51fe31261400
Piettrof <117880320+Piettrof@users.noreply.github.com>
2023-10-10 09:18:26 -0300

    simulação de matricula

--- desafio.kt
+++ desafio.kt
@@ -11,7 +11,10 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    inscritos.add(usuario)
+    ////TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+            println("Usuário ${usuario.nome} matriculado na formação $nome")
+            }
     }
 }
 
